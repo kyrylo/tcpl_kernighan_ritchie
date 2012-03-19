9876bd29d9966db076c83e4ce325641cd633a73f
Kyrylo Silin <kyrylosilin@gmail.com>
2012-03-20 01:05:55 +0200

    Add solution of 1-10

---
+++ chapter_1/1-10/input_to_output_with_substitutions.c
@@ -0,0 +1,21 @@
+/*
+ * Write a program to copy its input to its output, replacing each tab by \t,
+ * each backspace by \b, and each backslash by \\. This makes tabs and
+ * backspaces visible in an unambiguous way.
+ *
+ * Date: March 20, 2012
+ */
+
+#include <stdio.h>
+
+main() {
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\t') printf("\\t");
+    if (c == '\b') printf("\\b");
+    if (c == '\\') printf("\\\\");
+    else
+      putchar(c);
+  }
+}
